51281bd497b465aa54b951d9e03a7c65c6ef676b
tcnksm <nsd22843@gmail.com>
2016-07-21 12:00:47 +0900

    Skip check support stack

--- lib/dependencies.rb
+++ lib/dependencies.rb
@@ -80,7 +80,7 @@ module CompileExtensions
     end
 
     def dependency_satisfies_current_stack(dependency)
-      ENV['SKIP_CF_STACK_CHECK'] == "true" || dependency['cf_stacks'].include?(stack)
+      true
     end
 
     def stack
